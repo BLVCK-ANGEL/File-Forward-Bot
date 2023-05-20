0ec16ba9b6268c635b6e995ee36ee76120c3ad2e
BLVCK ANGEL <83200567+BLVCK-ANGEL@users.noreply.github.com>
2023-05-20 16:11:25 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -17,5 +17,5 @@ BOT_TOKEN = environ.get("BOT_TOKEN", "6189880153:AAH5M7ahk5rZJ6bk3Oo7G3rx6TKoZyo
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1001700545247"))
 PORT = int(environ.get("PORT", "8080"))
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '668692506').split()]
-TARGET_DB = int(environ.get("TARGET_DB", "-1001794250899"))
+TARGET_DB = int(environ.get("TARGET_DB", "-1001884385825"))
 UPSTREAM_REPO = environ.get("UPSTREAM_REPO", "https://github.com/BLVCK-ANGEL/File-Forward-Bot")
