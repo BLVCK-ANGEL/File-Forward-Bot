89abf60523397701b06ccbc79ae455752c360842
BLVCK ANGEL <83200567+BLVCK-ANGEL@users.noreply.github.com>
2023-09-24 10:44:24 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -13,9 +13,9 @@ def is_enabled(value, default):
 SESSION = environ.get("SESSION", "forward bot")
 API_ID = int(environ.get("API_ID", "16491027"))
 API_HASH = environ.get("API_HASH", "f98a18193ba4f65024671d973fea32d3")
-BOT_TOKEN = environ.get("BOT_TOKEN", "6189880153:AAH5M7ahk5rZJ6bk3Oo7G3rx6TKoZyoCajI")
+BOT_TOKEN = environ.get("BOT_TOKEN", "6309109185:AAEyVD9meHcNuBhxlmS6QuHkI3_guO2fGrU")
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1001700545247"))
 PORT = int(environ.get("PORT", "8080"))
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '668692506').split()]
-TARGET_DB = int(environ.get("TARGET_DB", "-1001884385825"))
+TARGET_DB = int(environ.get("TARGET_DB", "-1001970653510"))
 UPSTREAM_REPO = environ.get("UPSTREAM_REPO", "https://github.com/BLVCK-ANGEL/File-Forward-Bot")
